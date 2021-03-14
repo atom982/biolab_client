f4d544382c57d09157f62a4c80979172cf456926
ATOM | Lab Software <atom.bitbucket@gmail.com>
2021-03-14 15:07:25 +0100

    Build 14.3.021

--- src/components/statistika/Dashboard.vue
+++ src/components/statistika/Dashboard.vue
@@ -2,6 +2,11 @@
   <div class="dashboard">
     <dashboard-info-widgets></dashboard-info-widgets>
 
+
+    <div class="statistika">
+      <support></support>
+    </div>
+
     <div class="statistika">
       <data-visualisation-tab></data-visualisation-tab>
     </div>
@@ -12,12 +17,14 @@
 import Vue from "vue";
 import DashboardInfoWidgets from "./DashboardInfoWidgets";
 import DataVisualisationTab from "./DataVisualisation.vue";
+import Support from './Support.vue';
 
 export default {
   name: "dashboard",
   components: {
     DashboardInfoWidgets,
-    DataVisualisationTab
+    DataVisualisationTab,
+    Support
   }
 };
 </script>
